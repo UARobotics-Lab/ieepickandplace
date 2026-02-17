30f0de2f1de1fb7edc40e2560218d6533dd33984
Laura <lauraandrea2397@gmail.com>
2026-02-17 10:15:29 -0500

    Movimiento grip mano R/L funcionando

--- src/Sensor_tests.py
+++ src/Sensor_tests.py
@@ -1,84 +1,3 @@
-# import numpy as np
-# from unitree_sdk2py.core.channel import (
-#     ChannelSubscriber,
-#     ChannelFactoryInitialize,
-# )
-
-# from unitree_sdk2py.idl.unitree_hg.msg.dds_ import HandState_
-# from unitree_sdk2py.idl.default import unitree_hg_msg_dds__HandState_
-
-# # =========================
-# # CONFIG
-# # =========================
-# INVALID_VALUE = 30000
-# SCALE = 10000.0
-
-# # =========================
-# # DDS SETUP
-# # =========================
-# hand_id = input("Input hand (L/R): ").strip().upper()
-# iface = input("Network interface: ").strip()
-
-# topic = (
-#     "rt/lf/dex3/left/state"
-#     if hand_id == "L"
-#     else "rt/lf/dex3/right/state"
-# )
-
-# ChannelFactoryInitialize(0, iface)
-
-# subscriber = ChannelSubscriber(topic, HandState_)
-# state = unitree_hg_msg_dds__HandState_()
-
-# def cb(msg):
-#     global state
-#     state = msg
-
-# subscriber.Init(cb, 1)
-
-# print("\nEsperando datos de sensores...")
-# print("p = leer sensores | q = salir")
-
-# # =========================
-# # LECTURA
-# # =========================
-# def read_pressure():
-
-#     if len(state.press_sensor_state) == 0:
-#         print("No hay datos aún.")
-#         return
-
-#     print("\n==============================")
-
-#     for finger_idx, sensor in enumerate(state.press_sensor_state):
-
-#         raw = list(sensor.pressure)     # ← CLAVE
-#         temp = sensor.temperature
-#         lost = sensor.lost
-
-#         scaled = [
-#             0.0 if v == INVALID_VALUE else v / SCALE
-#             for v in raw
-#         ]
-
-#         print(f"\nFinger index: {finger_idx}")
-#         print("Lost packet:", lost)
-#         print("Temperature:", temp)
-#         print("Raw:", raw)
-#         print("Scaled:", np.round(scaled, 2))
-#         print("Max pressure:", round(max(scaled), 2))
-
-# # =========================
-# # LOOP
-# # =========================
-# while True:
-#     cmd = input().strip().lower()
-#     if cmd == "p":
-#         read_pressure()
-#     elif cmd == "q":
-#         print("Saliendo...")
-#         break
-
 import time
 import numpy as np
 from unitree_sdk2py.core.channel import (
@@ -128,7 +47,7 @@ while True:
             for v in raw
         ]
         print(
-            f"Finger {finger_idx} | "
+            f"ID {finger_idx} | "
             f"Max pressure: {round(max(scaled), 2)}"
         )
 
--- src/Test_hand.py
+++ src/Test_hand.py
@@ -145,13 +145,19 @@ def rotateMotors():
 
 
 def gripHand():
+
     maxL = maxLimits_left if isLeft else maxLimits_right
     minL = minLimits_left if isLeft else minLimits_right
 
+    if isLeft:
+        CLOSE_DIR = [0, +1, +1, -1, -1, -1, -1]
+    else:
+        CLOSE_DIR = [0, -1, -1, +1, +1, +1, +1]
+
+    # Posición media inicial
     for i in range(MOTOR_MAX):
         cmd_msg.motor_cmd[i].mode = build_mode(i, 1, 0)
         cmd_msg.motor_cmd[i].tau  = 0.0
-
         mid = (maxL[i] + minL[i]) / 2.0
         cmd_msg.motor_cmd[i].q  = mid
         cmd_msg.motor_cmd[i].dq = 0.0
@@ -162,26 +168,31 @@ def gripHand():
     time.sleep(1.0)
 
     array_move_factor = [0,0.1,0.2,0.3,0.4,0.3,0.2,0.1,0]
+
     for item in array_move_factor:
-        print(f"-- Actual state {item}")
+
         for i in range(MOTOR_MAX):
+
             cmd_msg.motor_cmd[i].mode = build_mode(i, 1, 0)
             cmd_msg.motor_cmd[i].tau  = 0.0
+
+            mid = (maxL[i] + minL[i]) / 2.0
+            range_amp = (maxL[i] - minL[i]) / 2.0
+
             if i == 0:
-                mid = (maxL[i] + minL[i]) / 2.0
+                q = mid
             else:
-                if i >= 1 and i <= 2:
-                    mid = (maxL[i] + minL[i]) / 2.0 - item * maxL[i]
-                else:
-                    mid = (maxL[i] + minL[i]) / 2.0 + item * maxL[i]
-            print(f"--Position value {mid}")
-            cmd_msg.motor_cmd[i].q  = mid
+                q = mid + CLOSE_DIR[i] * item * range_amp
+
+
+            cmd_msg.motor_cmd[i].q  = q
             cmd_msg.motor_cmd[i].dq = 0.0
             cmd_msg.motor_cmd[i].kp = 1.5
             cmd_msg.motor_cmd[i].kd = 0.1
 
         publisher.Write(cmd_msg)
         time.sleep(5.0)
+
 
 # =========================
 # STOP
